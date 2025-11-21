4d0e75f89ecc24dfd8a2266985070976df359e5f
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-11-21 13:29:00 -0800

    main component has started

--- gemini_clone/src/Components/Main_Bar/Main.jsx
+++ gemini_clone/src/Components/Main_Bar/Main.jsx
@@ -15,7 +15,43 @@ function Main() {
 
             </div>
 
+            
+
         </div>
+
+        <div className='Center_Box'>
+              <div className='hello_text'>
+                <p><span>
+                  Hey,Dev
+                </span></p>
+
+                <p>How Can I help you!</p>
+
+              </div>
+
+              <div className='Container'>
+                <div className='Card'>
+                  <p>Suggest beautiful places to see on an upcoming road trip</p>
+                  <img src={assets.compass_icon} alt="Compass" />
+                </div>
+
+                <div className='Card'>
+                  <p>Briefly Summarize this the concept: urban planning</p>
+                  <img src={assets.bulb_icon} alt="Bulb" />
+                </div>
+
+                <div className='Card'>
+                  <p>Brainstorm team banding activities for our work retreat</p>
+                  <img src={assets.message_icon} alt="Message" />
+                </div>
+
+                <div className='Card'>
+                  <p>Improve the readability of the following code</p>
+                  <img src={assets.code_icon} alt="Code_icons" />
+                </div>
+
+              </div>
+            </div>
 
     </div>
   )
