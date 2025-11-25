5bcfae7c5339dd8327f04ab9bf0c8c256eb350ee
Wogari-GH6877 <wogariararsa0968@gmail.com>
2025-11-25 13:24:16 -0800

    Context functionality

---
+++ gemini_clone/src/Context/Context.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+import {main} from "../Gemini_Config/GeminiConfig.jsx";
+import { useEffect } from "react";
+
+export const Appcontext=createContext();
+
+const ContextProvider =(props)=>{
+     
+  const [input,setInput]=useState("");
+  const [recentPrompt,setRecentPrompt]=useState("");
+  const [prevPrompt,setPrevPrompt]=useState([]);
+  const [showResult,setShowResult]=useState(false);
+  const [loading,setLoading]=useState(false);
+  const [resultData,setResutData]=useState("");
+
+   const onSent=async(prompt)=>
+
+   {  
+     setResutData("");
+     setLoading(true);
+     setShowResult(true);
+     setRecentPrompt(input);
+     const response= await main(input);
+     setResutData(response);
+     setLoading(false);
+     setInput("");
+}
+
+  
+
+
+    const contextValue={
+        input,setInput,recentPrompt,setRecentPrompt
+        ,prevPrompt,setPrevPrompt,showResult,setShowResult,
+        loading,setLoading,resultData,setResutData,onSent
+
+    }
+
+    return(
+        <Appcontext.Provider value={contextValue}>
+            {props.children}
+        </Appcontext.Provider>
+    )
+}
+export default ContextProvider
